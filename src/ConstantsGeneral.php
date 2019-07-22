02d93849e04eec7934ce3e9c721a9c127b2fb035
Ralf Zillmann <r.zillmann@sap.com>
2019-07-22 09:55:45 +0200

    yii2-rzhelper: remove instance constants
    Change-Id: I3e351df6fb007137a2dd566a7a2306d215f4ae95

--- src/ConstantsGeneral.php
+++ src/ConstantsGeneral.php
@@ -9,10 +9,6 @@ namespace rayn2k\rzhelper;
 class ConstantsGeneral
 {
 
-    const ACTIVE_INSTANCE_ID = 'active_instance_id';
-
-    const DEFAULT_INSTANCE_ID = 'default_instance_id';
-
     const SIGN_NOT_AVAILABLE = '-';
 }
 ?>
